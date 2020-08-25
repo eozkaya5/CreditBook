d68b7dc5fd4c453694ab9419b98f8024b30e001c
eozkaya5 <eozkaya675@gmail.com>
2020-08-26 02:32:59 +0300

    Ödeme sayfası için ödeme yap, sil ve listele tasarımları oluşturularak, işlemler eklendi

---
+++ CreditBook/Controllers/PaymentController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CreditBook.Models.BookViewModel;
+using CreditBook.Models.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CreditBook.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public readonly BookDbContext _context;
+
+        public PaymentController(BookDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(int id)
+        {
+            ViewBag.UserName = User.Identity.Name;
+            List<Payment> model = _context.Payments.Include(x => x.Customer).Where(x => x.CustomerId == id).ToList();
+            ViewBag.CustomerId = id;
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Create(int id)
+        {
+            var model = new Payment { CustomerId = id };
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Create(Payment model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var payment = _context.Customers.Find(model.CustomerId);
+                    payment.TotalDept -= model.FeePaid;
+                    model.Date = DateTime.Now;
+                    _context.Payments.Add(model);
+                    _context.SaveChanges();
+                    return RedirectToAction("Index", new { id = model.CustomerId });
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            return View(model);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var model = _context.Payments.Find(id);
+            _context.Payments.Remove(model);
+            _context.SaveChanges();
+            return RedirectToAction("Index", new { id = model.CustomerId });
+        }
+    }
+}
